486fe4501c9970cc9e4807f0664a894daeba400d
Ali Raza <81587307+Ali-Raza-099@users.noreply.github.com>
2023-02-07 20:40:38 +0500

    backend intialized, get route works

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+import connectToDatabase from './database.js';
+
+//Our Routes
+import productRouter from './routes/productRoutes.js';
+
+dotenv.config();
+connectToDatabase();
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.use('/api/products', productRouter);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
---
+++ server/routes/productRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import Product from '../models/Product.js';
+const productRouter = express.Router();
+
+const getProducts = async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+}
+
+productRouter.route('/').get(getProducts);
+
+export default productRouter;
